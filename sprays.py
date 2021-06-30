7b4844c8570d9a0ff1b2c98f74d3bbc7ff9d25f1
2021051285 <datmoyan@hanyang.ac.kr>
2021-06-30 15:03:36 +0900

    sprays.py update

--- sprays.py
+++ sprays.py
@@ -17,30 +17,29 @@ assert os.path.exists('./binaries/FileSystem_Stdio.dll'), '\'FileSystem_Stdio.dl
 
 count = 0
 
-for sprayFolderName in [dir for dir in os.listdir(IN_DIR) if os.path.isdir(dir)]:
-    sprayFolderPath = os.path.join(IN_DIR, sprayFolderName)
-    for sprayFileName in [spray for spray in os.listdir(sprayFolderPath)
-                            if os.path.isfile(os.path.join(sprayFolderPath), spray)
-                            and spray.endswith('.dat')]:
-        if (int(time.time()) - os.path.getmtime(os.path.join(dir, sprayFileName)) < 604800
-            and (not os.path.exists(os.path.join(OUT_DIR, os.path.splitext(sprayFileName)[0] + '.png'))
-                or int(time.time()) - os.path.getmtime(os.path.join(OUT_DIR, os.path.splitext(sprayFileName)[0] + '.png')) > 604800)):
-            beforePath = os.path.join(sprayFolderPath, sprayFileName)
-            afterPath = os.path.join('./temp/', os.path.splitext(sprayFileName)[0] + '.vtf')
+for sprayFolderPath in [path for path in map(lambda dir: os.path.join(IN_DIR, dir), os.listdir(IN_DIR)) if os.path.isdir(path)]:
+    for sprayFile in [spray for spray in map(lambda fileName: (fileName, os.path.join(sprayFolderPath, fileName), os.path.splitext(fileName)), os.listdir(sprayFolderPath))
+                            if os.path.isfile(spray[1])
+                            and spray[0].endswith('.dat')]:
+        if (int(time.time()) - os.path.getmtime(sprayFile[1]) < 604800
+            and (not os.path.exists(os.path.join(OUT_DIR, sprayFile[2][0] + '.png'))
+                or int(time.time()) - os.path.getmtime(os.path.join(OUT_DIR, sprayFile[2][0] + '.png')) > 604800)):
+            beforePath = sprayFile[1]
+            afterPath = os.path.join('temp/', sprayFile[2][0] + '.vtf')
             shutil.copyfile(beforePath, afterPath)
 
-            os.system(command.format(input='temp/' + os.path.splitext(sprayFileName)[0] + '.vtf', output='temp/temp'))
+            os.system(command.format(input=afterPath, output='temp/temp'))
 
             if os.path.exists('./temp/temp000.tga'):
                 img, *imgs = [Image.open(f) for f in sorted(glob.glob('./temp/temp*.tga'))]
-                img.save(fp=os.path.join(OUT_DIR, os.path.splitext(sprayFileName)[0] + '.png'), format='PNG', append_images=imgs, save_all=True, duration=200, loop=0)
+                img.save(fp=os.path.join(OUT_DIR, sprayFile[2][0] + '.png'), format='PNG', append_images=imgs, save_all=True, duration=200, loop=0)
 
                 for file in glob.glob('./temp/temp*.tga'):
                     os.remove(file)
 
             elif os.path.exists('./temp/temp.tga'):
                 img = Image.open('./temp/temp.tga')
-                img.save(fp=os.path.join(OUT_DIR, os.path.splitext(sprayFileName)[0] + '.png'), format='PNG')
+                img.save(fp=os.path.join(OUT_DIR, sprayFile[2][0] + '.png'), format='PNG')
 
                 os.remove('./temp/temp.tga')
 
