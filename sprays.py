fc76a717e93eedff8e1742c63dd56dfbd5982f07
Jobggun <jobggun@kakao.com>
2022-01-19 23:33:00 +0900

    refactored

--- sprays.py
+++ sprays.py
@@ -2,7 +2,6 @@ import os
 import glob
 import shutil
 from PIL import Image
-import time
 
 IN_DIR = '/image-in/user_custom/'
 OUT_DIR = '/image-out/'
@@ -17,35 +16,52 @@ assert os.path.exists('./binaries/FileSystem_Stdio.dll'), '\'FileSystem_Stdio.dl
 
 count = 0
 
-for sprayFolderPath in [path for path in map(lambda dir: os.path.join(IN_DIR, dir), os.listdir(IN_DIR)) if os.path.isdir(path)]:
-    for sprayFile in [spray for spray in map(lambda fileName: (fileName, os.path.join(sprayFolderPath, fileName), os.path.splitext(fileName)), os.listdir(sprayFolderPath))
-                            if os.path.isfile(spray[1])
-                            and spray[0].endswith('.dat')]:
-        if (int(time.time()) - os.path.getmtime(sprayFile[1]) < 604800
-            and (not os.path.exists(os.path.join(OUT_DIR, sprayFile[2][0] + '.png'))
-                or int(time.time()) - os.path.getmtime(os.path.join(OUT_DIR, sprayFile[2][0] + '.png')) > 604800)):
-            beforePath = sprayFile[1]
-            afterPath = os.path.join('temp/', sprayFile[2][0] + '.vtf')
-            shutil.copyfile(beforePath, afterPath)
+globStr = os.path.join(IN_DIR, '??/????????.dat')
 
-            os.system(command.format(input=afterPath, output='temp/temp'))
+for sprayFile in glob.iglob(globStr):
+    if not os.path.isfile(sprayFile):
+        continue
 
-            if os.path.exists('./temp/temp000.tga'):
-                img, *imgs = [Image.open(f) for f in sorted(glob.glob('./temp/temp*.tga'))]
-                img.save(fp=os.path.join(OUT_DIR, sprayFile[2][0] + '.png'), format='PNG', append_images=imgs, save_all=True, duration=200, loop=0)
+    path, name = os.path.split(sprayFile)
+    file, extension = os.path.splitext(name)
 
-                for file in glob.glob('./temp/temp*.tga'):
-                    os.remove(file)
+    destPath = os.path.join(OUT_DIR, file + '.png')
+    
+    if not os.path.exists(destPath):
+        pass
+    elif os.path.getmtime(sprayFile) > os.path.getmtime(destPath):
+        pass
+    else:
+        continue
 
-            elif os.path.exists('./temp/temp.tga'):
-                img = Image.open('./temp/temp.tga')
-                img.save(fp=os.path.join(OUT_DIR, sprayFile[2][0] + '.png'), format='PNG')
+    print('[INFO] IMAGE BEING PROCESSED NOW:', sprayFile)
+    
+    tempPath = os.path.join('temp', file + '.vtf')
+    shutil.copyfile(sprayFile, tempPath)
 
-                os.remove('./temp/temp.tga')
+    os.system(command.format(input=tempPath, output='temp/temp'))
 
-            try:
-                os.remove(afterPath)
-                count += 1
-                print(count)
-            except FileNotFoundError:
-                pass
+    if os.path.exists('temp/temp000.tga'):
+        img, *imgs = [Image.open(f) for f in sorted(glob.glob('temp/temp*.tga'))]
+        img.save(fp=destPath,
+                 format='PNG',
+                 append_images=imgs,
+                 save_all=True,
+                 duration=200,
+                 loop=0)
+        count += 1
+    
+    elif os.path.exists('temp/temp.tga'):
+        img = Image.open('temp/temp.tga')
+        img.save(fp=destPath, format='PNG')
+        count += 1
+    
+    for tgaFile in glob.iglob('temp/temp*.tga'):
+        os.remove(tgaFile)
+
+    try:
+        os.remove(tempPath)
+        print('[INFO] PROCESSED IMAGES COUNT:', count)
+        print()
+    except FileNotFoundError:
+        pass
