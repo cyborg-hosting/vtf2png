9dde4db08a1922ce9f8930bc905861ea7da57f1b
Jobggun <48022705+jobggun@users.noreply.github.com>
2023-03-06 11:19:54 +0900

    failsafe

--- sprays.py
+++ sprays.py
@@ -23,49 +23,55 @@ for src_path in INPUT_DIRECTORY.glob('??/????????.dat'):
     else:
         continue
 
-    
-    with open(src_path, 'rb') as file:
+    try:
         vtf = None
-
         try:
-            vtf = VTF.read(file)
-        except ValueError as e:
+            with open(src_path, 'rb') as file:
+
+                try:
+                    vtf = VTF.read(file)
+                except ValueError as e:
+                    print(f'[ERR] {src_path}: {e}', file=stderr)
+                    continue
+                
+                if vtf.flags & (VTFFlags.ONEBITALPHA | VTFFlags.EIGHTBITALPHA):
+                    if vtf.format == ImageFormats.DXT1:
+                        vtf.format = ImageFormats.DXT1_ONEBITALPHA
+                    if vtf.low_format == ImageFormats.DXT1:
+                        vtf.format = ImageFormats.DXT1_ONEBITALPHA
+                    for frame in vtf._frames.values():
+                        if frame._fileinfo is not None and frame._fileinfo[2] == ImageFormats.DXT1:
+                            frame._fileinfo = (frame._fileinfo[0], frame._fileinfo[1], ImageFormats.DXT1_ONEBITALPHA)
+
+                vtf.load()
+        except PermissionError as e:
             print(f'[ERR] {src_path}: {e}', file=stderr)
             continue
-        
-        if vtf.flags & (VTFFlags.ONEBITALPHA | VTFFlags.EIGHTBITALPHA):
-            if vtf.format == ImageFormats.DXT1:
-                vtf.format = ImageFormats.DXT1_ONEBITALPHA
-            if vtf.low_format == ImageFormats.DXT1:
-                vtf.format = ImageFormats.DXT1_ONEBITALPHA
-            for frame in vtf._frames.values():
-                if frame._fileinfo is not None and frame._fileinfo[2] == ImageFormats.DXT1:
-                    frame._fileinfo = (frame._fileinfo[0], frame._fileinfo[1], ImageFormats.DXT1_ONEBITALPHA)
 
-        vtf.load()
+        if vtf.frame_count <= 0:
+            continue
+        elif vtf.frame_count == 1:
+            img = vtf.get().to_PIL()
+            img.save(
+                dst_path,
+                format='PNG',
+                optimize=True,
+            )
+        else:
+            img, *imgs = ( vtf.get(frame=index).to_PIL() for index in range(vtf.frame_count) ) 
+            img.save(
+                dst_path,
+                format='PNG',
+                optimize=True,
+                append_images=imgs,
+                save_all=True,
+                duration=200,
+                loop=0,
+            )
+        print(f'[INFO] processed \'{src_path}\'')
 
-    if vtf.frame_count <= 0:
-        continue
-    elif vtf.frame_count == 1:
-        img = vtf.get().to_PIL()
-        img.save(
-            dst_path,
-            format='PNG',
-            optimize=True,
-        )
-    else:
-        img, *imgs = ( vtf.get(frame=index).to_PIL() for index in range(vtf.frame_count) ) 
-        img.save(
-            dst_path,
-            format='PNG',
-            optimize=True,
-            append_images=imgs,
-            save_all=True,
-            duration=200,
-            loop=0,
-        )
-    print(f'[INFO] processed \'{src_path}\'')
-
-    count += 1
+        count += 1
+    except Exception as e:
+        print(f'[ERR] {src_path}: {e.with_traceback()}', file=stderr)
 
 print('[INFO] total number of processed files:', count)
